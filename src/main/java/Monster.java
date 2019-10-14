0217470872c729501e4f375b72aba039dab766ab
Adan Anwar <adananwar71@gmail.com>
2019-10-14 22:02:32 +0200

    Added a Monster class and a Test class for Monster

---
+++ src/main/java/Monster.java
@@ -0,0 +1,10 @@
+public class Monster extends Character {
+
+    // Ska inte finnas namn eller XP, ska dock finnas "attack"
+    public Monster(String name, int xpLevel, int health) {
+        super(name, xpLevel, health);
+    }
+
+
+
+}
---
+++ src/test/java/MonsterTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterTest {
+
+    // Test klass för Monster liv
+
+    // Test klass för Monster damage
+
+}
