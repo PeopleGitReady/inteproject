d95d5811178a07a77e4adeea00329b30409447bb
phylyps <maohgren@gmail.com>
2019-10-15 16:40:45 +0200

    initiazations trimmed down

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -7,26 +7,26 @@ class PlayerTest {
 
     @Test
     void testIfNameContainsNumber() {
-        Player testCh = new Player("22.3", 20, 30, 50, 100, 44);
+        Player testCh = new Player("22.3");
         assertTrue(testCh.getName().matches(".*\\d.*"));
     }
 
     @Test
     void testNameNull(){
-        Player testCh = new Player(null,44,66, 100, 100, 50);
+        Player testCh = new Player(null);
         assertNotNull(testCh.getName());
     }
 
     @Test
     void testNameEmpty(){
         //nameEmpty method should have a boolean return type
-        Player testCh = new Player("     ",44,66, 100, 100, 59);
+        Player testCh = new Player("     ");
         assertFalse(testCh.getName().trim().isEmpty());
     }
 
     @Test
     void testNameString(){
-        Player testCh = new Player("SavageB",44,66, 100, 100, 50);
+        Player testCh = new Player("SavageB");
         assertEquals("SavageB",testCh.getName());
     }
 
@@ -35,20 +35,19 @@ class PlayerTest {
     void testHealthNumberRangeNull(){
         //healthRangeCorrect should return a boolean.
         int x = Integer.parseInt(null);
-        Player testCh = new Player("SavageB",44, x, 100, 100, 50);
-        assertFalse(testCh.healthRangeCorrect(testCh.getHealth());
+        Player testCh = new Player("SavageB");
+        assertFalse(testCh.healthRangeCorrect(testCh.getHealth()));
     }
 
     @Test
     void testHealthNumberRangeNegative(){
-        Player testCh = new Player("SavageB",44,-99, 11, 100, 110);
-        assertTrue(testCh.healthRangeCorrect(testCh.getHealth());
+        Player testCh = new Player("SavageB");
+        assertTrue(testCh.healthRangeCorrect(testCh.getHealth()));
     }
     @Test
     void testHealthNumberRangeAboveRange(){
-        Player testCh = new Player("SavageB",44,1000, 233, 100, 11);
-        assertTrue(testCh.healthRangeCorrect(testCh.getHealth));
-
+        Player testCh = new Player("SavageB");
+        assertTrue(testCh.healthRangeCorrect(testCh.getHealth()));
 
     @Test
     void pickUpItemWhenInventoryFull() {
