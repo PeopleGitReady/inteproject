7f90fa711d1745a0a7f72aca5292eb8ddfab099f
moslehmahamud <mosleh.edu@gmail.com>
2019-10-15 10:42:45 +0200

    Added a attack construktor

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,19 +1,25 @@
 public class Character {
 
     private String name;
-    private int level;
+    private int xpLevel;
     private int health;
+    private int attack;
 
-    public Character(String name, int level, int health) {
+    public Character(String name, int xpLevel, int health, int attack) {
         this.name = name;
-        this.level = level;
+        this.xpLevel = xpLevel;
         this.health = health;
+        this.attack=attack;
     }
 
 
     public void movement(){
 
     }
+
+
+
+
 
 
 
