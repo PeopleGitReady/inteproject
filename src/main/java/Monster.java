b39fe711af19a4a5d63ecd22a098ca9069f9831b
Adan Anwar <adananwar71@gmail.com>
2019-10-16 16:03:40 +0200

    Added a method for when a Monster takes damage

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-
 public class Monster extends Character {
 
     //Monster bawser = new Monster("Bawser", 100, 10, 10);
@@ -12,14 +10,13 @@ public class Monster extends Character {
 
     // Metod som kollar om Monster är död så droppa item på dess plats
 
-    public int monsterDeadDamage (int decreaseHealth) {
-        int health
-        bawser.setHealth(); -= decreaseHealth;
-        if(bawser.getHealth() <= 0) {
-            bawser.setIsDead(true);
-            System.out.println("RIP");
-            Point deadLocation = bawser.getPoint().getLocation();
-            Item item = new Item(deadLocation);
+    public void monsterTakeDamage (int attack) {
+       int newHealth = getHealth() - attack;
+       setHealth(newHealth);
+        if (getHealth() <= 0) {
+            setIsDead(true);
+            generateDrop(getPoint());
+        }
         }
     }
-}
+
