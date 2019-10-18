691e611b0d9faa3632bf3be3f6f1c07bb36fdc5c
Adan Anwar <adananwar71@gmail.com>
2019-10-18 13:08:34 +0200

    Fixed player move Y

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -183,7 +183,7 @@ class PlayerTest {
     void playerMoveX() {
         Player p = new Player("Playah");
         assertEquals(0, p.getPoint().getX());
-        //p.moveX(1);
+        p.moveX();
         assertEquals(1, p.getPoint().getX());
     }
 
@@ -192,7 +192,7 @@ class PlayerTest {
     void playerMoveY() {
         Player p = new Player("Playah");
         assertEquals(0, p.getPoint().getX());
-       // p.moveY(1);
+        p.moveY();
         assertEquals(1, p.getPoint().getY());
     }
 
