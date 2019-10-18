9d7cf4859ef0f0008f1642da3108d3a1efb219c1
Adan Anwar <adananwar71@gmail.com>
2019-10-18 11:34:33 +0200

    Removed unnecessary constructor in the Item class

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -5,10 +5,6 @@ public abstract class Item {
     private String name;
     private Point point;
 
-    public Item(Point point) {
-        this.point = point;
-    }
-
     public Item(String name, Point point) {
         this.name = name;
         this.point = point;
